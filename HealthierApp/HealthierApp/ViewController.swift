1ad0ffbbd63a9eca7d8aa0fb17acec8f1f65ed53
Raul Sulaimanov <raulsulaimanov@admins-MBP.fritz.box>
2021-08-10 16:52:19 +0200

    recreated the xcode workspace

---
+++ HealthierApp/HealthierApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthierApp
+//
+//  Created by Raul Sulaimanov on 10.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
